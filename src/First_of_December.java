d8f41d54d96bc36d62cf4fe9c6f4c564a6250682
RolandRepanoviciProjects <rolandrepanovici.gaming@gmail.com>
2022-12-10 21:26:35 +0100

    Completed the 1st day challenge.

--- src/First_of_December.java
+++ src/First_of_December.java
@@ -1,23 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class First_of_December {
     public static void main(String[] args) throws Exception {
-        String calories = """
-            1000
-            2000
-            3000
-            
-            4000
-            
-            5000
-            6000
-            
-            7000
-            8000
-            9000
-            
-            10000
-                """;
-        for (String s : calories.split("\n")){
-            System.out.println(s);
-        }
+        BufferedReader br = new BufferedReader(new FileReader("Input_Files/1 of December.txt"));
+
+                int calorie = 0;
+                int mostCalories = 0;
+
+                List<Integer> calorieList = new ArrayList<>();
+                
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    String strCal = line.trim();
+                    if (strCal.isBlank()) {
+                        if (mostCalories < calorie) {
+                            mostCalories = calorie;
+                        }
+                        calorieList.add(calorie);
+                        calorie = 0;
+                        continue;
+                    }
+                    calorie += Integer.parseInt(strCal);
+                }
+
+                calorieList.add(calorie);
+                if(mostCalories < calorie){
+                    mostCalories = calorie;
+                }
+
+                System.out.println(mostCalories);
+
+                calorieList.sort((a, b) -> b - a);
+                System.out.println(calorieList.get(0) + calorieList.get(1) + calorieList.get(2));
+    br.close(); 
+                 
     }
 }
