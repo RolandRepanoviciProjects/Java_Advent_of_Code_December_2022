da30487ce8254b67606da713de969ef6e718e872
RolandRepanoviciProjects <rolandrepanovici.gaming@gmail.com>
2022-12-10 19:39:48 +0100

    Test comit

--- src/First_of_December.java
+++ src/First_of_December.java
@@ -18,6 +18,7 @@ public class First_of_December {
                 """;
         for (String s : calories.split("\n")){
             System.out.println(s);
+            System.out.println("test");
         }
     }
 }
