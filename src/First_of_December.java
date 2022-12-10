d313855029049ff79f21f827edaff7bc31de00e0
RolandRepanoviciProjects <rolandrepanovici.gaming@gmail.com>
2022-12-10 19:36:37 +0100

    First comit for the 1. of December: Initial string

---
+++ src/First_of_December.java
@@ -0,0 +1,23 @@
+public class First_of_December {
+    public static void main(String[] args) throws Exception {
+        String calories = """
+            1000
+            2000
+            3000
+            
+            4000
+            
+            5000
+            6000
+            
+            7000
+            8000
+            9000
+            
+            10000
+                """;
+        for (String s : calories.split("\n")){
+            System.out.println(s);
+        }
+    }
+}
